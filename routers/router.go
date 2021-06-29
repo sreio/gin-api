2d05e9f5adeef66f639adc028b5f5a1c0b5afe81
xingwei <xingwei@optaim.com>
2021-06-29 18:59:06 +0800

    article

--- routers/router.go
+++ routers/router.go
@@ -22,6 +22,17 @@ func InitRouter() *gin.Engine {
 		apiv1.POST("/tags", v1.AddTag) //新建标签
 		apiv1.PUT("/tags/:id", v1.UpdateTag) // 更新标签
 		apiv1.DELETE("/tags/:id", v1.DeleteTag) //删除标签
+
+		//获取文章列表
+        apiv1.GET("/articles", v1.GetArticles)
+        //获取指定文章
+        apiv1.GET("/articles/:id", v1.GetArticle)
+        //新建文章
+        apiv1.POST("/articles", v1.AddArticle)
+        //更新指定文章
+        apiv1.PUT("/articles/:id", v1.EditArticle)
+        //删除指定文章
+        apiv1.DELETE("/articles/:id", v1.DeleteArticle)
 	}
 
     return r
